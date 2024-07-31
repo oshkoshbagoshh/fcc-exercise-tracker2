4f2c368c65db7bbd555ae2f1a6f35b5846e2a158
AJ Javadi <amirjavadi25@gmail.com>
2024-07-31 15:00:27 -0500

    exercise and logs working propertly

--- models/Exercise.js
+++ models/Exercise.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const exerciseSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
         required: true,
-        unique: true
     },
     description: {
         type: String,
